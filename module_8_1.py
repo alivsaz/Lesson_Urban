144f337f7094875c4426207d477a529875f0866b
Alex <alivsaz@gmail.com>
2024-10-26 20:56:05 +0300

    Update module_8_1.py - добавил описание ошибки

--- module_8_1.py
+++ module_8_1.py
@@ -3,7 +3,8 @@
 def add_everything_up(a, b):
     try:
         return a + b
-    except TypeError:
+    except TypeError as exc:
+        print(f'Ошибка - {exc}: Попытка сложить разные типы данных')
         return str(a) + str(b)
 
 print(add_everything_up(123.456, 'строка'))
