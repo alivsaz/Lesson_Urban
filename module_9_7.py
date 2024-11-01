89e255812ea0c8699cb19fa526934bd03b769153
Alex <alivsaz@gmail.com>
2024-11-01 13:18:29 +0300

    Update module_9_7.py - оптимизация перебора до квадратного корня числа

--- module_9_7.py
+++ module_9_7.py
@@ -3,7 +3,7 @@
 def is_prime(func):
     def wrapper(*args):
         count, n = 0, func(*args)
-        for i in range(2, n):
+        for i in range(2, int(n**0.5) + 1):    # перебор чисел до корня из числа
             if n % i == 0:
                 count += 1
         if count > 0:
